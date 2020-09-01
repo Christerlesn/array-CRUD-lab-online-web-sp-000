392f4d279573bb9f215e041d0c37849c45df522b
Christerle Nounoute <christerlesn@gmail.com>
2020-09-01 19:36:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 array = create_an_array
- array << "the final element, 5"
+ array << element
 end
 
 def add_element_to_start_of_array(array, element)
